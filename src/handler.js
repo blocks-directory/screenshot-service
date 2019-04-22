d638a422232c71178d760f0d3459a756edc93c78
edraitsev <ddrrai@gmail.com>
2019-04-22 14:14:22 +0200

    Simplified puppeteer setup step

---
+++ src/handler.js
@@ -0,0 +1,40 @@
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
+
+exports.default = async (event) => {
+  try {
+    const { url, width = 1500, height = 900 } = event.queryStringParameters || {}
+
+    if (!url) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ massage: 'you should specify page URL' }),
+      }
+    }
+
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    })
+
+    const page = await browser.newPage()
+    page.setViewport({ width: Number(width), height: Number(height) })
+    await page.goto(url)
+    const imageBuffer = await page.screenshot()
+
+    return {
+      statusCode: 200,
+      body: imageBuffer.toString('base64'),
+      isBase64Encoded: true,
+      headers: { 'Content-Type': 'image/png' },
+    }
+  } catch (e) {
+    console.log(e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ massage: 'Something going wrong on backend side' }),
+    }
+  }
+}
